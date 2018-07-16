7fef39831fed016ee242d74e11cfd212ae17f0ee
Ashfaq Aziz <ashfaqaziz16@gmail.com>
2018-07-16 04:32:54 +0000

    classwork 7

--- server.js
+++ server.js
@@ -7,6 +7,14 @@
   app.get('/', function(req, res){
      res.sendFile(__dirname+'/about.html');
   });
+  
+   app.get('/', function(req, res){
+     res.sendFile(__dirname+'/index.html');
+  });
+  
+    app.get('/', function(req, res){
+     res.sendFile(__dirname+'/Form.html');
+  });
 
 server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
